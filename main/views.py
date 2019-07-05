43355eec17f0624e12795ea11eeba8cd885f15cf
W-eye <w-eye@mail.ru>
2019-07-05 08:21:57 +0300

    Roles templates updates

--- main/views.py
+++ main/views.py
@@ -3,7 +3,7 @@ from django.views.generic.base import TemplateView
 from django.views.generic.edit import CreateView
 from django.contrib.auth import views as auth_views
 from .forms import ProviderForm
-from .models import Provider
+from .models import Provider, Roles
 
 
 # Create your views here.
@@ -23,16 +23,18 @@ from .models import Provider
 class IndexView(TemplateView):
     template_name = 'main/index.html'
 
-    def get(self, request, *args, **kwargs):
-
-
-        return super().get(request, *args, **kwargs)
+    def __init__(self):
+        self.user = None
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['providers'] = Provider.objects.all()
+        context['user_role'] = self.user.role
         return context
 
+    def dispatch(self, request, *args, **kwargs):
+        self.user = request.user
+        return super().dispatch(request, *args, **kwargs)
 
 
 
