5af39b4c6851491402d2cac4a67aa6cb6b1d70bd
Artem <evrikaseven@gmail.com>
2019-07-17 12:10:17 +0300

    Added new order view for zakazschik

--- main/roles/zakazschik/urls.py
+++ main/roles/zakazschik/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import ZakazschikMainView
+from .views import *
 
 
 urlpatterns = [
     path('main/', ZakazschikMainView.as_view(), name=ZakazschikMainView.url_name),
+    path('new_order/', NewOrderView.as_view(), name='new_order'),
 ]
--- main/roles/zakazschik/views.py
+++ main/roles/zakazschik/views.py
@@ -5,3 +5,11 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 class ZakazschikMainView(LoginRequiredMixin, TemplateView):
     template_name = 'main/zakazschik.html'
     url_name = 'zakazschik'
+
+
+class NewOrderView(TemplateView):
+    template_name = 'main/new_order.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
