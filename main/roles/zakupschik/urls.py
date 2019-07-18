e1966f3be411d257f6a9b9958e598a98c2a43aba
Discipilus <w-eye@mail.ru>
2019-07-18 15:40:58 +0300

    Zakupschik updated

--- main/roles/zakupschik/urls.py
+++ main/roles/zakupschik/urls.py
@@ -6,6 +6,8 @@ urlpatterns = [
     path('main/', views.ZakupschikMainView.as_view(), name=views.ZakupschikMainView.url_name),
     path('individual_orders/', views.ZakupschikIndividualOrdersView.as_view(),
          name=views.ZakupschikIndividualOrdersView.url_name),
+    path('individual_orders/<slug:place>/', views.ZakupschikOrdersByPlacesView.as_view(),
+         name=views.ZakupschikOrdersByPlacesView.url_name),
     path('joint_orders/', views.ZakupschikJointOrdersView.as_view(),
          name=views.ZakupschikJointOrdersView.url_name),
 ]
