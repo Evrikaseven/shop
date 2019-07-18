14b85b7e8e3606bd10d84ebbdbb860c181ea6f9d
Artem Zamulin <artem.zamulin@revelsystems.com>
2019-07-18 15:35:31 +0300

    Added order list view

--- main/roles/zakazschik/urls.py
+++ main/roles/zakazschik/urls.py
@@ -5,4 +5,5 @@ from .views import *
 urlpatterns = [
     path('main/', ZakazschikMainView.as_view(), name=ZakazschikMainView.url_name),
     path('new_order/', NewOrderView.as_view(), name='new_order'),
+    path('orders_list/', OrdersListView.as_view(), name='orders_list'),
 ]
--- main/roles/zakazschik/views.py
+++ main/roles/zakazschik/views.py
@@ -1,9 +1,9 @@
 from django.views.generic.base import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
-
+from main import models as _models
 from django.views.generic.edit import CreateView
 
-from main.forms import OrderForm
+from main.forms import OrderForm, ProviderForm
 from django.urls import reverse_lazy
 
 
@@ -20,4 +20,13 @@ class NewOrderView(CreateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['form'] = self.form_class
+        return context
+
+
+class OrdersListView(TemplateView):
+    template_name = 'main/orders_list.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['orders'] = _models.Order.objects.all()
         return context
