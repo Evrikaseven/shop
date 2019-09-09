38561899ab47abaaa0785bb9c3811cf2a960b30d
Artem Zamulin <artem.zamulin@revelsystems.com>
2019-09-09 18:31:28 +0300

    Second update for UI

--- main/static/js/name_to_photo.js
+++ main/static/js/name_to_photo.js
@@ -3,9 +3,11 @@ $('.custom-file-input').on('change', function() {
    let fileName = $(this).val().split('\\').pop();
    $(this).next('.custom-file-label').addClass("selected").html(fileName);
 });
-$(function() {
-  $('#order').change(function(){
-    let sum =  parseInt($("#price-input").val(), 10) * parseInt($("#count-input").val(), 10);
-    $('#check_sum').html( sum + sum / 10);
-  });
-});
+
+
+// $(function() {
+//   $('#order').change(function(){
+//     let sum =  parseInt($("#price-input").val(), 10) * parseInt($("#count-input").val(), 10);
+//     $('#check_sum').html( sum + sum / 10);
+//   });
+// });
